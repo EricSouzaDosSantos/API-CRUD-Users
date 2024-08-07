33a5cb37cc9bff614047e42bda97bd2a2c6a2e19
Eric <ericsouza94895@gmail.com>
2024-08-07 20:27:01 -0300

    feat: creation of the method responsible for formatting the characters of the birthday date

--- crud/src/main/java/com/user/crud/service/UserService.java
+++ crud/src/main/java/com/user/crud/service/UserService.java
@@ -9,4 +9,9 @@ public class UserService {
         cpf = cpf.replaceAll("[^0-9]", ""); // Remover caracteres não numéricos
         return cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
     }
+
+    public static String FormatBirthDate(String birth){
+        birth = birth.replaceAll("[^0-9]", ""); // Remover caracteres não numéricos
+        return birth.replaceAll("(\\d{2})(\\d{2})(\\d{4})", "$1/$2/$3");
+    }
 }
