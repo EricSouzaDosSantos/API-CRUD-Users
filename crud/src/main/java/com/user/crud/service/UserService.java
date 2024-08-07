eb4312bf7083a4bf407c95ab459f49673bd9935c
Eric <ericsouza94895@gmail.com>
2024-08-07 20:25:42 -0300

    feat: creation of the method responsible for formatting the CPF characters

--- crud/src/main/java/com/user/crud/service/UserService.java
+++ crud/src/main/java/com/user/crud/service/UserService.java
@@ -1,2 +1,12 @@
-package com.user.crud.service;public class UserService {
+package com.user.crud.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public static String FormatCPF(String cpf){
+        cpf = cpf.replaceAll("[^0-9]", ""); // Remover caracteres não numéricos
+        return cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+    }
 }
