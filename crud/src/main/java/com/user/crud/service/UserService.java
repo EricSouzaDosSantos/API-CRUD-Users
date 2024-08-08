ce37e5bbfdeee82ab0238fa0d239182c1d0d09f3
Eric <ericsouza94895@gmail.com>
2024-08-08 17:20:26 -0300

    fix: I removed comments that were not being useful

--- crud/src/main/java/com/user/crud/service/UserService.java
+++ crud/src/main/java/com/user/crud/service/UserService.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     public static String FormatCPF(String cpf){
-        cpf = cpf.replaceAll("[^0-9]", ""); // Remover caracteres não numéricos
+        cpf = cpf.replaceAll("[^0-9]", "");
         return cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
     }
 
     public static String FormatBirthDate(String birth){
-        birth = birth.replaceAll("[^0-9]", ""); // Remover caracteres não numéricos
+        birth = birth.replaceAll("[^0-9]", "");
         return birth.replaceAll("(\\d{2})(\\d{2})(\\d{4})", "$1/$2/$3");
     }
 }
